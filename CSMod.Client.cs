d127f4d2697f0e24185443c3ca7eb0cd5d9d795a
HellGoesOn <35145123+HellGoesOn@users.noreply.github.com>
2020-03-05 20:34:45 +0300

    Buy menu.

--- CSMod.Client.cs
+++ CSMod.Client.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CounterStrike.UserInterfaces.BuyMenu;
 using CounterStrike.UserInterfaces.KillFeed;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -12,6 +13,7 @@ namespace CounterStrike
         private void LoadClient()
         {
             KillFeedLayer = new KillFeedLayer(new KillFeedUIState());
+            BuyLayer = new BuyLayer(new BuyMenuState());
 
             Reload = RegisterHotKey("Reload", Keys.G.ToString());
         }
@@ -24,16 +26,20 @@ namespace CounterStrike
 
         public override void UpdateUI(GameTime gameTime)
         {
+            BuyLayer?.Update(gameTime);
             KillFeedLayer?.Update(gameTime);
         }
 
         public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
         {
             layers.Add(KillFeedLayer);
+            layers.Add(BuyLayer);
         }
 
 
         public KillFeedLayer KillFeedLayer { get; private set; }
+
+        public BuyLayer BuyLayer { get; private set; }
 
         public ModHotKey Reload { get; private set; }
     }
---
+++ UserInterfaces/BuyMenu/BuyLayer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.UI;
+
+namespace CounterStrike.UserInterfaces.BuyMenu
+{
+    public class BuyLayer : GameInterfaceLayer
+    {
+        public BuyLayer(BuyMenuState state) : base("CounterStrike: Buy Menu", InterfaceScaleType.UI)
+        {
+            BuyUIState = state;
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            BuyUIState?.Update(gameTime);
+        }
+
+        protected override bool DrawSelf()
+        {
+            if(BuyUIState != null && BuyUIState.Visible)
+                BuyUIState?.Draw(Main.spriteBatch);
+
+            return true;
+        }
+
+
+        public BuyMenuState BuyUIState { get; } 
+    }
+}
